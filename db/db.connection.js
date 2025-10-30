624af75e164857a899886a99c0f7ae0c127236fa
parshuram8884 <106910128+parshuram8884@users.noreply.github.com>
2025-10-30 17:23:11 +0530

    Remove sensitive MongoDB connection string
    Updated MongoDB connection string to an empty string for security reasons.

--- db/db.connection.js
+++ db/db.connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async() => {
     try {
-        const connect = await mongoose.connect("mongodb+srv://mohitparshuramthakur234:0SWOPfmcYl4BKGRj@cluster0.trij3.mongodb.net/?appName=Cluster0/DBName")
+        const connect = await mongoose.connect("")
         console.log(`connection sucessfully ${connect.connection.host}`)
     } catch (error) {
         console.error("database connect failed Try again")
